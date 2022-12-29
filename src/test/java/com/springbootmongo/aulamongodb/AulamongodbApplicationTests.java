b96ae82adcbc1c185d767e3d5e7357cdb0d55163
Eliton <eliton.dev.25@gmail.com>
2022-12-28 22:14:22 -0300

    Projeto criado

---
+++ src/test/java/com/springbootmongo/aulamongodb/AulamongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootmongo.aulamongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulamongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
