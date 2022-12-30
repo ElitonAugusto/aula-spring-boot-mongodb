eb60e8ed6c40e515164b4d56304c82e5a36c5c93
Eliton <eliton.dev.25@gmail.com>
2022-12-29 22:24:49 -0300

    Consulta simples com @Query

--- src/main/java/com/springbootmongo/aulamongodb/repository/PostRepository.java
+++ src/main/java/com/springbootmongo/aulamongodb/repository/PostRepository.java
@@ -3,11 +3,16 @@ package com.springbootmongo.aulamongodb.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.springbootmongo.aulamongodb.domain.Post;
 
 
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
