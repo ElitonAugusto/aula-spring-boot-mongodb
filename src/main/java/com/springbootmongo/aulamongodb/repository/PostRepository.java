f4ec76a06b5c9f7872d777588d403e3efcc30068
Eliton <eliton.dev.25@gmail.com>
2022-12-29 22:17:03 -0300

    Consulta simples com query methods

--- src/main/java/com/springbootmongo/aulamongodb/repository/PostRepository.java
+++ src/main/java/com/springbootmongo/aulamongodb/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.springbootmongo.aulamongodb.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.springbootmongo.aulamongodb.domain.Post;
@@ -7,4 +9,5 @@ import com.springbootmongo.aulamongodb.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
