fe2dec54abcbff6a43f9e428e5b1c398de04265b
Eliton <eliton.dev.25@gmail.com>
2022-12-29 21:02:32 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/springbootmongo/aulamongodb/resources/UserResource.java
+++ src/main/java/com/springbootmongo/aulamongodb/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.springbootmongo.aulamongodb.domain.Post;
 import com.springbootmongo.aulamongodb.domain.User;
 import com.springbootmongo.aulamongodb.dto.UserDTO;
 import com.springbootmongo.aulamongodb.services.UserService;
@@ -62,4 +63,10 @@ public class UserResource {
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}	
+	
+	@GetMapping("/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
 }
